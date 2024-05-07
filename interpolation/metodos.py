37233b6eafa55cab9463b03e12ed6a54016c4046
unknown <simobotero7445@gmail.com>
2024-05-07 13:53:41 -0500

    definir entradas de metódos de interpolación y creación de metodos.py

---
+++ interpolation/metodos.py
@@ -0,0 +1,8 @@
+def vandermonde(puntos):
+    return
+
+def newton_inter(puntos):
+    return
+
+def lagrange(puntos):
+    return
--- interpolation/views.py
+++ interpolation/views.py
@@ -1,11 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from . import metodos
 
 # Create your views here.
 def vandermonde_view(request):
-    return render(request, 'interpolation/vandermonde.html')
+    
+    if request.method == 'POST':
+        puntos = {"x": [], "y": []}
+        punto_str = request.POST.get('punto')
+        lista=(punto_str.split(' '))
+        for element in lista:
+            punto=eval(element)
+            x,y=punto
+            puntos["x"].append(x)
+            puntos["y"].append(y)
 
+        coordenadas=zip(puntos["x"], puntos["y"])   
+        return render(request, 'interpolation/vandermonde.html', {'coordenadas': coordenadas})
+    else:
+        return render(request, 'interpolation/vandermonde.html')
+    
 def newton_inter_view(request):
-    return render(request, 'interpolation/newton_inter.html')
+    
+    if request.method == 'POST':
+        puntos = {"x": [], "y": []}
+        punto_str = request.POST.get('punto')
+        lista=(punto_str.split(' '))
+        for element in lista:
+            punto=eval(element)
+            x,y=punto
+            puntos["x"].append(x)
+            puntos["y"].append(y)
+
+        coordenadas=zip(puntos["x"], puntos["y"])   
+        return render(request, 'interpolation/newton_inter.html', {'coordenadas': coordenadas})
+    else:
+        return render(request, 'interpolation/newton_inter.html')
 
 def lagrange_view(request):
-    return render(request, 'interpolation/lagrange.html')
+    
+    if request.method == 'POST':
+        puntos = {"x": [], "y": []}
+        punto_str = request.POST.get('punto')
+        lista=(punto_str.split(' '))
+        for element in lista:
+            punto=eval(element)
+            x,y=punto
+            puntos["x"].append(x)
+            puntos["y"].append(y)
+
+        coordenadas=zip(puntos["x"], puntos["y"])   
+        return render(request, 'interpolation/lagrange.html', {'coordenadas': coordenadas})
+    else:
+        return render(request, 'interpolation/lagrange.html')
