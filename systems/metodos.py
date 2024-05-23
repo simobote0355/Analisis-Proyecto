5b8ba09e875999138ccb9d8b5a66b95ff983ce0d
unknown <simobotero7445@gmail.com>
2024-05-23 17:24:58 -0500

    crear metodo de jacobi

--- systems/metodos.py
+++ systems/metodos.py
@@ -1,5 +1,43 @@
-def jacobi(A,b):
-    return
+import numpy as np
+import pandas as pd
+
+def jacobi(a, b, x0, tol, iter_max, norma):
+    diagonal = np.diag(np.diag(a))
+    inferior = -np.tril(a, k=-1)
+    superior = -np.triu(a, k=1)
+    
+    inversa_d = np.linalg.inv(diagonal)
+    t = inversa_d @ (inferior + superior)
+    c = inversa_d @ b
+
+    i = 0
+    errores = []
+    soluciones = []
+    iter=[]
+    iter.append(i)
+    while i < iter_max:
+        x = t @ x0 + c
+    
+        error = np.linalg.norm(x - x0, ord=norma)
+        errores.append(error)
+        soluciones.append(x)
+        
+        if error < tol:
+            mensaje=f"La solución ha convergido {x} con una tolerancia de {tol} después de {i+1} iteraciones."
+            break
+        
+        x0 = x
+        i += 1  
+        iter.append(i)
+    
+    else:
+        mensaje=f"El método no converge después de {iter_max} iteraciones."
+
+    tabla = pd.DataFrame({'Iteración': [i for i in range(len(soluciones))],
+                               'Error': errores,
+                               'Solución': soluciones})
+    
+    return tabla, mensaje
 
 def gauss_seidel(A,b):
     return
--- systems/views.py
+++ systems/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 import random
+import numpy as np
 from . import metodos
+from django.shortcuts import render
 
 # Create your views here.
 def jacobi_view(request):
     if request.method == 'POST':
         n = int(request.POST.get('n'))
-        matriz = [[random.randint(1, 100) for _ in range(n)] for _ in range(n)]
-        b = [random.randint(1, 100) for _ in range(n)]
-        return render(request, 'systems/jacobi.html', {'matriz': matriz, 'b': b})
+        tol = float(request.POST.get('tol'))
+        max_iter = int(request.POST.get('max_iter'))
+        norma = int(request.POST.get('norma'))
+        matriz = np.random.randint(1, 100, size=(n, n))
+        b = np.random.randint(1, 100, size=n)
+        x0 = np.array([0,0,0])
+
+        print(matriz)
+        print(b)
+        print(x0)
+        tabla, mensaje = metodos.jacobi(matriz, b, x0, tol, max_iter, norma)
+        return render(request, 'systems/jacobi.html', {'matriz': matriz, 'b': b, 'x0': x0, 'tabla': tabla.to_html(), 'mensaje': mensaje})
     else:
         return render(request, 'systems/jacobi.html')
 
